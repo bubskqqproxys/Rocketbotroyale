ecfb8e832a9b3fd0a3c33a7f57dce37f330b61de
bubskqqproxys <168247419+bubskqqproxys@users.noreply.github.com>
2024-04-27 19:44:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require("express")
-const https = require("https")
-const app = express()
+const express = require("express");
+const https = require("https");
+const app = express();
 
 app.get("/chess/*", (req, res, next) => {
   const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[2]
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
+  const discordPath = pathSegments[2];
+  const url = "https://chess.com/" + discordPath;
+  forwardRequest(url, req, res);
+});
+
 app.get("/*", (req, res, next) => {
-  //const pathSegments = req.path.split('/');
- // const rocketPath = pathSegments[2]
-  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
+  const url = "https://rocketbotroyale.winterpixel.io/" + req.path;
+  forwardRequest(url, req, res);
+});
 
+function forwardRequest(url, req, res) {
+  https.request(new URL(url), (resp) => {
+    res.contentType(resp.headers["content-type"]);
+    resp.pipe(res);
+  }).end();
+}
 
-app.listen(3000)
+app.listen(3000);
